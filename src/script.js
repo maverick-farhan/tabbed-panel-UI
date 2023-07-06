eb49d10c4cbdb0da21d5decc0e2a77ecb1d7dee0
maverick-farhan <mdfarhanofficial@gmail.com>
2023-07-06 10:14:55 +0530

    Done

--- src/script.js
+++ src/script.js
@@ -1,9 +1,13 @@
-const tabLists = document.querySelectorAll('.tab-list');
-tabLists.forEach((tabList)=>{
-    tabList.addEventListener('click',function(e){
-        e.preventDefault();
-        let links = this.firstChild;
-        let activeTab = tabList.querySelectorAll(`:scope ${'li.active'}`)
-        console.log(this.firstChild);
-    })
+const tabPanels = document.querySelectorAll('div.tab-panel');
+const tabControls =  document.querySelectorAll('li a.tab-control')
+tabControls.forEach((tab,i)=>{
+    tab.addEventListener('click',function(e){
+        tabControls.forEach(tab=>{tab.parentElement.classList.remove('active')});
+        tab.parentElement.classList.add('active');
+        tabPanels.forEach(content=>{
+        content.classList.remove('active');});
+        tabPanels[i].classList.add('active')
+    }
+    
+    )
 })
